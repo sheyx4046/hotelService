a6a656e2261fe68fc9fc854266f3382860a48736
shuhrat <qayumov7298668@gmail.com>
2023-11-03 22:13:37 +0500

    fixing form of values

--- hotel/hotel_thymeleaf_security/src/main/java/com/example/hotel_thymeleaf_security/entity/dtos/FindDto.java
+++ hotel/hotel_thymeleaf_security/src/main/java/com/example/hotel_thymeleaf_security/entity/dtos/FindDto.java
@@ -2,11 +2,7 @@ package com.example.hotel_thymeleaf_security.entity.dtos;
 
 import lombok.Data;
 
-import java.time.LocalDate;
-
 @Data
 public class FindDto {
-    LocalDate from, to;
-    String city;
-    double minPrice, maxPrice;
+    String from, to, city,minPrice, maxPrice;
 }
