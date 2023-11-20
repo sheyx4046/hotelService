63b2ea23896485b89337ec28a46572105c76729b
KShuhrat <qayumov7298668@gmail.com>
2023-11-21 00:37:56 +0500

    small changes

--- hotel/hotel_thymeleaf_security/src/main/java/com/example/hotel_thymeleaf_security/controller/HomeController.java
+++ hotel/hotel_thymeleaf_security/src/main/java/com/example/hotel_thymeleaf_security/controller/HomeController.java
@@ -69,7 +69,7 @@ public class HomeController {
             @RequestParam("size")Optional<Integer> size
             ) {
         int currentPage = page.orElse(1);
-        int pageSize = size.orElse(10);
+        int pageSize = size.orElse(9);
         Page<VillaRentEntity> villas = villageService.getAllPage(PageRequest.of(currentPage - 1, pageSize));
         model.addAttribute("villas", villas);
         int totalPages = villas.getTotalPages();
