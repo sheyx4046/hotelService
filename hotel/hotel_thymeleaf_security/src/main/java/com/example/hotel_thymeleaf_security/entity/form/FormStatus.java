e7ff044bd3c06a50e59ea2df5a94d52601443c55
kshukhrat <qayumov7298668@gmail.com>
2023-12-11 12:20:09 +0500

    changes in html - use thymeleaf fragment

---
+++ hotel/hotel_thymeleaf_security/src/main/java/com/example/hotel_thymeleaf_security/entity/form/FormStatus.java
@@ -0,0 +1,6 @@
+package com.example.hotel_thymeleaf_security.entity.form;
+
+public enum FormStatus {
+    READIED, NOT_READIED;
+
+}
--- hotel/hotel_thymeleaf_security/src/main/java/com/example/hotel_thymeleaf_security/service/user/form/FormService.java
+++ hotel/hotel_thymeleaf_security/src/main/java/com/example/hotel_thymeleaf_security/service/user/form/FormService.java
@@ -3,6 +3,14 @@ package com.example.hotel_thymeleaf_security.service.user.form;
 import com.example.hotel_thymeleaf_security.entity.dtos.FormDto;
 import com.example.hotel_thymeleaf_security.entity.form.Form;
 import com.example.hotel_thymeleaf_security.service.BaseService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.UUID;
 
 public interface FormService extends BaseService<Form, FormDto> {
+    Page<Form> getAll(Pageable pageable);
+
+    Form viewFormById(UUID formId);
 }
